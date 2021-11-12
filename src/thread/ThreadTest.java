59898599aa100a27513375cf587f8888997870d6
gogoing-code <1378412050@qq.com>
2021-11-12 23:29:54 +0800

    代码修改

--- src/thread/ThreadTest.java
+++ src/thread/ThreadTest.java
@@ -4,16 +4,18 @@ package thread;/*
 
 class MThread extends Thread{
     private int num = 100;
+//    Object object = new Object();
+
 
     @Override
     public void run(){
         while(true){
-            if(num > 0){
-                num--;
-                System.out.println(Thread.currentThread().getName() + "票数剩下" + num);
-            }else{
-                break;
-            }
+                if(num > 0){
+                    num--;
+                    System.out.println(Thread.currentThread().getName() + "票数剩下" + num);
+                }else{
+                    break;
+                }
         }
     }
 }
@@ -21,8 +23,11 @@ public class ThreadTest {
     public static void main(String[] args) {
 
         MThread myThread1 = new MThread();
+        myThread1.setName("线程1");
         MThread myThread2 = new MThread();
+        myThread2.setName("线程2");
         MThread myThread3 = new MThread();
+        myThread3.setName("线程3");
         myThread1.start();
         myThread2.start();
         myThread3.start();
